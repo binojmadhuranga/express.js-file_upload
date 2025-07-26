ab91897facbd45b5d0caa82b3a7e6b8751b33a74
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2025-07-26 10:36:49 +0530

    feat: add dotenv package with support for environment variable management
    - Introduced a new dotenv package to load environment variables from a .env file.
    - Added TypeScript definitions for better type safety and developer experience.
    - Implemented CLI options parsing to customize dotenv behavior via command line.
    - Added support for encrypted .env.vault files with decryption capabilities.
    - Included security contact information in SECURITY.md.
    - Enhanced documentation and examples for usage in README.

--- controller/customer-controller.js
+++ controller/customer-controller.js
@@ -1,19 +1,19 @@
 const e = require("express");
-
+require('dotenv').config();
 const mysql = require('mysql2');
 
 // Create connection
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '1235', 
-  database: 'mydatabase' 
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 
 
 const updateCustomer = (req, res) => {
-   
+
     const { name, address, contact_number } = req.body;
     connection.query('UPDATE customer SET name = ?, address = ?, contact_number = ? WHERE id = ?', [name, address, contact_number, req.params.id], (error, results) => {
         if (error) {
@@ -25,7 +25,7 @@ const updateCustomer = (req, res) => {
 
 
 const deleteCustomer = (req, res) => {
-   
+
     connection.query('DELETE FROM customer WHERE id = ?', [req.params.id], (error, results) => {
         if (error) {
             return res.status(500).send("Error deleting customer");
@@ -54,21 +54,21 @@ const getCustomerByid = (req, res) => {
 
 
 const createCustomer = (req, res) => {
- connection.query('INSERT INTO customer (name, address,  contact_number) VALUES (?, ?, ?)', [req.body.name, req.body.address, req.body.contact_number], (error, results) => {
-    if (error) {
-      return res.status(500).send("Error creating customer");
-    }
-   else {
-      res.status(201).send("Customer created successfully!");
-    }
-  });
+    connection.query('INSERT INTO customer (name, address,  contact_number) VALUES (?, ?, ?)', [req.body.name, req.body.address, req.body.contact_number], (error, results) => {
+        if (error) {
+            return res.status(500).send("Error creating customer");
+        }
+        else {
+            res.status(201).send("Customer created successfully!");
+        }
+    });
 };
 
 
 module.exports = {
-  updateCustomer,
-  deleteCustomer,
-  getCustomer,
-  getCustomerByid,
-  createCustomer
+    updateCustomer,
+    deleteCustomer,
+    getCustomer,
+    getCustomerByid,
+    createCustomer
 };
